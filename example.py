d55ee6a6787b3e94dceb58ee910246cd0fc820bb
Hugo Fonseca <hugo.fonseca01101000@gmail.com>
2026-07-09 16:56:50 -0300

    aula 4 anotacoes

--- example.py
+++ example.py
@@ -1,11 +1,2 @@
-print("Digite um número:")
-a = int(input())
-if (a % 2) == 0 and (a < 100):
-    print("O número é par e menor do que 100")
-elif (a % 2 == 0) and (a >= 100):
-    print("O número é par e maior ou igual que 100")
-elif (a % 2 != 0) and (a < 100):
-    print("O número é ímpar e menor do que 100")
-else:
-#elif a % 2 != 0 and a >= 100:
-    print("O número é ímpar e maior ou igual que 100")
+for i in list(range(2)):
+    print("x")
